10e2ec29ea309d85f4db0b9d1682b6180a6bbe2e
William Novak <williamnvk@gmail.com>
2019-10-03 14:44:31 -0300

    change modal component

--- site/tab.js
+++ site/tab.js
@@ -7,61 +7,54 @@ applyIndicatorClass(tabs[0]);
 displaySelectedTab(tabsContent[0].dataset.index);
 
 function drag(event) {
-    event.target.classList.add("button--is-dragged");
+  event.target.classList.add("button--is-dragged");
 }
 
 function drop(event) {
-    console.log("Drop");
-    event.target.classList.remove("button--is-dragged");
+  console.log("Drop");
+  event.target.classList.remove("button--is-dragged");
 }
 
 for (let tab of tabs) {
-    tab.addEventListener("click", (event) => {
-        applyIndicatorClass(event.target);
-        displaySelectedTab(event.target.dataset.index);
-    })
+  tab.addEventListener("click", (event) => {
+    applyIndicatorClass(event.target);
+    displaySelectedTab(event.target.dataset.index);
+  })
 }
 
 function applyIndicatorClass(target) {
-    console.log("apply");
-    for (let tab of tabs){
-        if(tab.dataset.index == target.dataset.index) {
-            tab.classList.add("tab__item--has-indicator");
-        } else {
-            tab.classList.remove("tab__item--has-indicator");
-        }
+  console.log("apply");
+  for (let tab of tabs) {
+    if (tab.dataset.index == target.dataset.index) {
+      tab.classList.add("tab__item--has-indicator");
+    } else {
+      tab.classList.remove("tab__item--has-indicator");
     }
+  }
 }
 
 function displaySelectedTab(selectedTable) {
-    for (let content of tabsContent){
-        if (selectedTable == content.dataset.index) {
-            content.style.display = "block"
-        } else {
-            content.style.display = "none"
-        }
+  for (let content of tabsContent) {
+    if (selectedTable == content.dataset.index) {
+      content.style.display = "block"
+    } else {
+      content.style.display = "none"
     }
+  }
 }
 
-     
+
 const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.modal__card__close');
-const closeBtn = document.querySelector('.modal__card__footer__close');
+const closeModal = document.querySelector('.modal__close');
 
-closeModal.addEventListener("click", () => {
-    document.documentElement.style.overflow = 'scroll';
-    document.body.scroll = "yes";
-  modal.classList.add("modal--is-hidden");
-})
-
-closeBtn.addEventListener("click", () => {
-    document.documentElement.style.overflow = 'scroll';
-    document.body.scroll = "yes";
-  modal.classList.add("modal--is-hidden");
-})
+// closeModal.addEventListener("click", () => {
+//   document.documentElement.style.overflow = 'scroll';
+//   document.body.scroll = "yes";
+//   modal.classList.add("modal--is-hidden");
+// })
 
 document.querySelector("#openModal").addEventListener("click", () => {
-    document.documentElement.style.overflow = 'hidden';
-    document.body.scroll = "no";
-    modal.classList.remove("modal--is-hidden")
+  document.documentElement.style.overflow = 'hidden';
+  document.body.scroll = "no";
+  modal.classList.remove("modal--is-hidden")
 })
