39219b78a46484e808703228535e50cd2fa5ae42
William Novak <williamnvk@gmail.com>
2019-10-16 08:28:56 -0300

    add sass

--- examples/with-typescript/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Components } from 'venom'
-import { ButtonComponent } from 'venom'
-
-Components.initialize()
-
-// const button = new ButtonComponent(document.querySelector(''))
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
 const { src, dest } = require('gulp')
 const replace = require('gulp-replace')
 
-exports.default = function () {
-
-  console.log("\x1b[31m", `
+exports.default = function() {
+  console.log(
+    '\x1b[31m',
+    `
   ██   ██  ██████  ██████  ██  ███████  ██████████    ██   ██  ██
   ██   ██  ██      ██  ██  ██  ██   ██  ██  ██  ██    ██   ██  ██
   ██   ██  ██ ███  ██  ██  ██  ██   ██  ██  ██  ██    ██   ██  ██
    █   █   ██      ██  ██  ██  ██   ██  ██  ██  ██    ██   ██  ██
     ███    ██████  ██  ██████  ███████  ██  ██  ██    ███████  ██
-  `)
+  `
+  )
 
-  src('resources/fonts/**/*')
-    .pipe(dest('dist/fonts'))
-
-  return src('resources/icons/**/*')
-    .pipe(dest('dist/icons'))
+  src('resources/fonts/**/*').pipe(dest('dist/fonts'))
+  src('sass/**/*').pipe(dest('dist/sass'))
+  src('venom.sass').pipe(dest('dist'))
+  return src('resources/icons/**/*').pipe(dest('dist/icons'))
 
   // return src([('css/venom.min.css')])
   //   .pipe(replace('helper', ''))
